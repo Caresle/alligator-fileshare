d7b6a23c96d915da5e266d7a7c7f9795bcc9cbbb
Caresle <carabesle@gmail.com>
2025-01-06 20:27:00 -0600

    feat(frontend): add query keys and item to states

--- frontend/src/app/_states/useFileDelete.ts
+++ frontend/src/app/_states/useFileDelete.ts
@@ -1,11 +1,14 @@
 import { create } from "zustand"
+import { FileEntity } from "../../entities/file.entity"
 
 interface FileDeleteState {
 	show: boolean
+	item: FileEntity
 	update: (state: Partial<FileDeleteState>) => void
 }
 
 export const useFileDeleteState = create<FileDeleteState>(set => ({
 	show: false,
+	item: {} as FileEntity,
 	update: data => set(data),
 }))
--- frontend/src/app/_states/useFileState.ts
+++ frontend/src/app/_states/useFileState.ts
@@ -1,11 +1,14 @@
 import { create } from "zustand"
+import { FileEntity } from "../../entities/file.entity"
 
 interface FileState {
 	show: boolean
+	item: FileEntity
 	update: (state: Partial<FileState>) => void
 }
 
 export const useFileState = create<FileState>(set => ({
 	show: false,
+	item: {} as FileEntity,
 	update: data => set(data),
 }))
