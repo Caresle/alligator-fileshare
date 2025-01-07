dd753765a251f1d98fb962c3553944265f045780
Caresle <carabesle@gmail.com>
2025-01-06 20:26:28 -0600

    feat(frontend): add tanstack provider

--- frontend/src/providers/providers.tsx
+++ frontend/src/providers/providers.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import { NextUIProvider } from "@nextui-org/react"
+import TanstackProvider from "./transtack-query"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return <NextUIProvider>{children}</NextUIProvider>
+	return (
+		<NextUIProvider>
+			<TanstackProvider>{children}</TanstackProvider>
+		</NextUIProvider>
+	)
 }
