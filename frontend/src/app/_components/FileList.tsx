c24e97e183c6776e3682294e7e0e0c1bce0c2e07
Caresle <carabesle@gmail.com>
2025-01-06 20:27:39 -0600

    feat(frontend): add fetch of files

--- frontend/src/app/_components/FileList.tsx
+++ frontend/src/app/_components/FileList.tsx
@@ -8,8 +8,13 @@ import {
 } from "@nextui-org/react"
 import { useFileDeleteState } from "../_states/useFileDelete"
 import { useFileState } from "../_states/useFileState"
+import { useQuery } from "@tanstack/react-query"
+import { queryKeys } from "../../constants/queryKeys"
+import { fileService } from "../../services/file.service"
+import { useMemo } from "react"
+import { FileEntity } from "../../entities/file.entity"
 
-const Actions = () => {
+const Actions = ({ item }: { item: FileEntity }) => {
 	const { update } = useFileDeleteState(state => state)
 	const { update: updateFile } = useFileState(state => state)
 	return (
@@ -27,14 +32,14 @@ const Actions = () => {
 					Download
 				</DropdownItem>
 				<DropdownItem
-					onPress={() => updateFile({ show: true })}
+					onPress={() => updateFile({ show: true, item })}
 					key={2}
 					startContent={<Icons.Actions.Edit className="size-5" />}
 				>
 					Rename
 				</DropdownItem>
 				<DropdownItem
-					onPress={() => update({ show: true })}
+					onPress={() => update({ show: true, item })}
 					key={3}
 					startContent={<Icons.Actions.Delete className="size-5" />}
 				>
@@ -46,6 +51,17 @@ const Actions = () => {
 }
 
 export default function FileList() {
+	const QFiles = useQuery({
+		queryKey: [queryKeys.FILES],
+		queryFn: fileService.get.all,
+	})
+
+	const data = useMemo(() => {
+		return QFiles?.data ?? []
+	}, [QFiles.data])
+
+	if (QFiles.isLoading || QFiles.isRefetching) return <div>Loading...</div>
+
 	return (
 		<section className="flex-1 bg-slate-200 p-2 rounded-lg flex flex-col overflow-y-auto">
 			<h1 className="text-2xl font-semibold">List of files</h1>
@@ -54,17 +70,18 @@ export default function FileList() {
             sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
         "
 			>
-				{Array.from({ length: 100 }).map((_, i) => (
+				{data.map((file, i) => (
 					<div
 						key={i}
-						className="flex items-center gap-2 p-2 bg-white rounded-lg w-full hover:cursor-pointer"
+						className="flex items-center gap-2 p-2 bg-white rounded-lg w-full hover:cursor-pointer h-fit"
 					>
 						<Icons.FileTypes.Image className="size-12" />
 						<div className="flex-1 flex flex-col w-full truncate">
-							<div className="text-sm font-semibold truncate">{i}.png</div>
-							<div className="text-xs text-slate-400">{i} MB</div>
+							<div className="text-sm font-semibold truncate">
+								{file.fileName}
+							</div>
 						</div>
-						<Actions />
+						<Actions item={file} />
 					</div>
 				))}
 			</div>
