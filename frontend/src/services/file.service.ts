c8697e4c5856b03e1657945149b920c199ad0759
Caresle <carabesle@gmail.com>
2025-01-11 21:20:14 -0600

    feat(backend): add download the file Fixes #4

--- frontend/src/services/file.service.ts
+++ frontend/src/services/file.service.ts
@@ -14,6 +14,26 @@ export const fileService = {
 				return []
 			}
 		},
+		async one(id: string): Promise<void> {
+			try {
+				const response = await client.get(`${BASE_ROUTE}/${id}`, {
+					responseType: "blob",
+				})
+
+				const url = window.URL.createObjectURL(new Blob([response.data]))
+				const link = document.createElement("a")
+				link.href = url
+				link.setAttribute(
+					"download",
+					`${Date.now().toString()}.${response.data.type?.split("/")?.[1]}`
+				)
+				document.body.appendChild(link)
+				link.click()
+				link.remove()
+			} catch (error) {
+				console.error(error)
+			}
+		},
 	},
 	post: {
 		one: async (formData: FormData) => {
